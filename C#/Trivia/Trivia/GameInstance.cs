4fec9c7a96353828ae2cb4928639c1352e416432
Scott Edwards <scotteza@gmail.com>
2018-08-08 11:09:24 +0100

    Added golden master.

---
+++ C#/Trivia/Trivia/GameInstance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UglyTrivia;
+
+namespace Trivia
+{
+    public class GameInstance
+    {
+        private static bool notAWinner;
+
+        public void RunGameSimulation(int? seed)
+        {
+            Game aGame = new Game();
+
+            aGame.add("Chet");
+            aGame.add("Pat");
+            aGame.add("Sue");
+
+            Random rand = seed.HasValue ? new Random(seed.Value) : new Random();
+
+            do
+            {
+                aGame.roll(rand.Next(5) + 1);
+
+                if (rand.Next(9) == 7)
+                {
+                    notAWinner = aGame.wrongAnswer();
+                }
+                else
+                {
+                    notAWinner = aGame.wasCorrectlyAnswered();
+                }
+            } while (notAWinner);
+        }
+    }
+}
---
+++ C#/Trivia/Trivia/GoldenMasterTests.cs
@@ -0,0 +1,30 @@
+﻿using ApprovalTests;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+using System;
+using System.IO;
+
+namespace Trivia
+{
+    [TestFixture]
+    public class GoldenMasterTests
+    {
+        [Test]
+        [UseReporter(typeof(DiffReporter))]
+        public void Run_Golden_Master()
+        {
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            for (var i = 0; i < 1000; i++)
+            {
+                var gameInstance = new GameInstance();
+                gameInstance.RunGameSimulation(i);
+            }
+            var output = stringWriter.ToString();
+
+
+            Approvals.Verify(output);
+        }
+    }
+}
